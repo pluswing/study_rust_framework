c9a2d3e8809a67322fe2a2611b03e02be63d667f
pluswing <toshiyuki@pluswing.co.jp>
2023-10-25 10:12:26 +0900

    #002 path parameter

--- hello-actix-web/src/main.rs
+++ hello-actix-web/src/main.rs
@@ -1,3 +1,5 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -11,4 +13,33 @@ async fn echo(req_body: String) -> impl Responder {
 
 async fn manual_hello() -> impl Responder {
   HttpResponse::Ok().body("Hey there!")
+}
+
+#[get("/users/{user_id}/{friend}")]
+async fn friend(path: web::Path<(u32, String)>) -> Result<String> {
+  let (user_id, friend) = path.into_inner();
+  Ok(format!("Wrlcome {}, user_id {}", friend, user_id));
+}
+
+#[derive(Deserialize)]
+struct Info {
+  user_id: u32,
+  friend: String,
+}
+
+#[get("/users2/{user_id}/{friend}")]
+async fn friend2(info: web::Path<Info>) -> Result<String> {
+  Ok(format!("Wrlcome {}, user_id {}", info.friend, info.user_id));
+}
+
+
+async fn main() -> std::io::Result<()> {
+  HttpServer::new(|| {
+    App::new()
+      .service(hello)
+      .service(echo)
+      .route("/hey", web::get().to(manual_hello))
+  }).bind(("127.0.0.1", 8080))?
+  .run()
+  .await
 }
