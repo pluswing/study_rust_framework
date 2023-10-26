36cbadb7aa0e9a8cbf08dc4edea4d74859dfa435
toshiyuki <toshiyuki.saito.16@gmail.com>
2023-10-26 10:19:00 +0900

    #003 parse json request

--- hello-actix-web/src/main.rs
+++ hello-actix-web/src/main.rs
@@ -32,10 +32,46 @@ async fn friend2(info: web::Path<Info>) -> Result<String> {
   Ok(format!("Wrlcome {}, user_id {}", info.friend, info.user_id));
 }
 
+#[derive(Deserialize)]
+struct Info2 {
+    username: String,
+}
+
+#[get("/q")]
+async fn query(info: web::Query<Info2>) -> String {
+    format!("Welcome {}!", info.username)
+}
+
+#[derive(Deserialize)]
+struct Info3 {
+    username: String,
+}
+
+#[post("/submit")]
+async fn submit(info: web::Json<Info3>) -> Result<String> {
+    Ok(format!("Welcome {}!", info.username))
+}
+
+// content-type multipart/form-data
+#[derive(Deserialize)]
+struct FormData {
+    username: String,
+}
+
+#[post("/form")]
+async fn index(form: web::Form<FormData>) -> Result<String> {
+    Ok(format!("Welcome {}!", form.username))
+}
 
 async fn main() -> std::io::Result<()> {
   HttpServer::new(|| {
+    let json_config = web::JSonConfig::default()
+      .limit(4096)
+      .error_handle(|err, _req| {
+        error::InternalError::from_response(err, HttpResponse::Conflict().finish()).into()
+      });
     App::new()
+      .app_data(json_config)
       .service(hello)
       .service(echo)
       .route("/hey", web::get().to(manual_hello))
