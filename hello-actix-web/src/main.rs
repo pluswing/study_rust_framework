c8f1d60184896318b1edddc69ae204ccee905ce7
pluswing <toshiyuki@pluswing.co.jp>
2023-10-27 10:15:07 +0900

    #004 json response, run server

--- hello-actix-web/src/main.rs
+++ hello-actix-web/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
 use serde::Deserialize;
 
 #[get("/")]
@@ -14,7 +14,7 @@ async fn echo(req_body: String) -> impl Responder {
 async fn manual_hello() -> impl Responder {
   HttpResponse::Ok().body("Hey there!")
 }
-
+/*
 #[get("/users/{user_id}/{friend}")]
 async fn friend(path: web::Path<(u32, String)>) -> Result<String> {
   let (user_id, friend) = path.into_inner();
@@ -63,15 +63,36 @@ async fn index(form: web::Form<FormData>) -> Result<String> {
     Ok(format!("Welcome {}!", form.username))
 }
 
+struct MyObj {
+  name: &'static str,
+}
+
+impl Responder for MyObj {
+  type Body = BoxBody;
+
+  fn respond_to(self, req: &actix_web::HttpRequest) -> HttpResponse<Self::Body> {
+      let body = serde_json::to_string(&self).unwrap();
+      HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(body)
+  }
+}
+
+async fn json_resp() -> impl Responder {
+  MyObj { name: "user" }
+}
+*/
+
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
   HttpServer::new(|| {
-    let json_config = web::JSonConfig::default()
-      .limit(4096)
-      .error_handle(|err, _req| {
-        error::InternalError::from_response(err, HttpResponse::Conflict().finish()).into()
-      });
+    // let json_config = web::JSonConfig::default()
+    //   .limit(4096)
+    //   .error_handle(|err, _req| {
+    //     error::InternalError::from_response(err, HttpResponse::Conflict().finish()).into()
+    //   });
     App::new()
-      .app_data(json_config)
+      // .app_data(json_config)
       .service(hello)
       .service(echo)
       .route("/hey", web::get().to(manual_hello))
