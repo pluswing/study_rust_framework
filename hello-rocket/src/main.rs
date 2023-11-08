65c2f56cc9cbb3eac465878af0f5a2247014960a
pluswing <toshiyuki@pluswing.co.jp>
2023-11-08 10:00:08 +0900

    #010 rocketでjsonリクエスト、レスポンス

--- hello-rocket/src/main.rs
+++ hello-rocket/src/main.rs
@@ -1,3 +1,5 @@
+use rocket::serde::{Deserialize, Serialize, json::Json};
+
 #[macro_use] extern crate rocket;
 
 #[get("/world")]
@@ -10,9 +12,29 @@ fn index2() -> String {
   "Hello, world!".to_string()
 }
 
+#[get("/hello/<name>/<age>/<cool>")]
+fn hello(name: &str, age: u8, cool: bool) -> String {
+    if cool {
+        format!("You're a cool {} year old, {}!", age, name)
+    } else {
+        format!("{}, we need to talk about your coolness.", name)
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Task {
+    description: String,
+    complete: bool
+}
+
+#[post("/todo", data = "<task>")]
+fn todo(task: Json<Task>) -> Json<Task> {
+  Json(Task { description: "aaaa".to_string(), complete: true })
+ }
+
 #[launch]
 fn rocket() -> _ {
   rocket::build()
-  .mount("/hello", routes![world, index2])
-  .mount("/hi", routes![world, index2])
+  .mount("/", routes![world, index2, hello, todo])
 }
